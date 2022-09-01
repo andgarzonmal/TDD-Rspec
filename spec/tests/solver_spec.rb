eb7eae72735e8721837fd7026368a9f449ded446
oyelakinG9 <oyelakinr37@gmail.com>
2022-09-01 21:47:31 +0100

    Modify solver spec test file

--- spec/tests/solver_spec.rb
+++ spec/tests/solver_spec.rb
@@ -10,4 +10,16 @@ describe Solver do
   it 'factorial method raises an exeption if user pass negative int' do
     expect(solver.factorial(-1)).to raise_error(ArgumentError)
   end
+
+  it 'factorial number of 5 should return 120' do
+    expect(solver.factorial(5)).to eq(120)
+  end
+
+  it 'factorial number of 1 should return 1' do
+    expect(solver.factorial(1)).to eq(1)
+  end
+
+  it 'factorial number of 0 should return 1' do
+    expect(solver.factorial(0)).to eq(1)
+  end
 end
