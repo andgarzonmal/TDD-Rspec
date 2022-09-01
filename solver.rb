b17fd866236546b88585f2d5f20baac032014348
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-01 16:19:55 -0500

    fix linters

--- solver.rb
+++ solver.rb
@@ -1,9 +1,10 @@
 class Solver
   def factorial(int)
     factorial = 1
-    raise ArgumentError, "please input a positive int" if int.negative?
+    raise ArgumentError, 'please input a positive int' if int.negative?
     return 1 if int.zero?
-    int.downto(1) {|number| factorial *= number }
+
+    int.downto(1) { |number| factorial *= number }
     factorial
   end
 
