d144106775eed8761d0f1f22c405f60133b7ef7f
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-01 16:05:14 -0500

    refactor tests

--- spec/tests/solver_spec.rb
+++ spec/tests/solver_spec.rb
@@ -4,11 +4,11 @@ describe Solver do
   solver = Solver.new
   
   it 'its a instance of solver' do 
-    expect(solver).to be_instance Solver
+    expect(solver).to be_instance_of Solver
   end
   
   it 'factorial method raises an exeption if user pass negative int' do
-    expect(solver.factorial(-1)).to raise_error(ArgumentError)
+    expect {solver.factorial(-1)}.to raise_error(ArgumentError)
   end
 
   it 'factorial number of 5 should return 120' do
