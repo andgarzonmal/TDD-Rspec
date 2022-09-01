514c44ac9c357d0be384b1d8c7262cc55f33e556
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-01 16:05:55 -0500

    Add factorial method and test it

---
+++ solver.rb
@@ -0,0 +1,9 @@
+class Solver
+  def factorial(int)
+    factorial = 1
+    raise ArgumentError, "please input a positive int" if int.negative?
+    return 1 if int.zero?
+    int.downto(1) {|number| factorial *= number }
+    factorial
+  end
+end
